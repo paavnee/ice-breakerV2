73d885ea6e3ddd99bdacc0fd890f095e0396bc1f
pavni <pavni2406@gmail.com>
2023-07-04 22:03:42 +0530

    added all files

---
+++ ice_breaker/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+print(chain.run(information=information))
+    # linkedin_profil_url = linkedin_lookup_agent(name="Eden Marco")
+    # print(linkedin_profil_url)
+    # linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://gist.githubusercontent.com/paavnee/0cf94303b542d1b14c80047c4a6235d8/raw/863dd70c0e1bb8c5fa9f231aea2477fae4a36ba6/williamhgates")
+    # # linkedin_profile_url="https://www.linkedin.com/in/williamhgates"
+    # print(chain.run(information=linkedin_data))
